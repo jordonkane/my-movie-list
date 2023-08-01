4e33589dd34a086648113d38832604df41697d9c
Jordon Kane <kanejord99@outlook.com>
2023-08-01 19:14:00 -0400

    Built AppReducer which returns state data
    The AppReducer describes how the state is transfers into the next state. Tells the store what to do with the data when something has happened.

---
+++ src/context/AppReducer.js
@@ -0,0 +1,44 @@
+/*
+    The AppReducer describes how the state is transfers into the next state.
+    Tells the store what to do with the data when something has happened.
+*/
+
+export default (state, action) => {
+  switch (action.type) { // Tell the reducer how to change the state with 'action'
+    case "ADD_MOVIE_TO_WATCHLIST":
+      return {
+        ...state,
+        watchlist: [action.payload, ...state.watchlist],
+      };
+    case "REMOVE_MOVIE_FROM_WATCHLIST":
+      return {
+        ...state,
+        watchlist: state.watchlist.filter(
+          (movie) => movie.id !== action.payload
+        ),
+      };
+    case "ADD_MOVIE_TO_WATCHED":
+      return {
+        ...state,
+        watchlist: state.watchlist.filter(
+          (movie) => movie.id !== action.payload.id
+        ),
+        watched: [action.payload, ...state.watched],
+      };
+    case "MOVE_TO_WATCHLIST":
+      return {
+        ...state,
+        watched: state.watched.filter(
+          (movie) => movie.id !== action.payload.id
+        ),
+        watchlist: [action.payload, ...state.watchlist],
+      };
+    case "REMOVE_FROM_WATCHED":
+      return {
+        ...state,
+        watched: state.watched.filter((movie) => movie.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
