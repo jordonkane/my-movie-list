5e4751bcec2e96d34611716a28139fb63c0bbcd5
Jordon Kane <kanejord99@outlook.com>
2023-08-02 16:04:03 -0400

    Added movie controls

---
+++ src/components/MovieControls.js
@@ -0,0 +1,46 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+// Controls for updating movie lists
+export const MovieControls = ({ type, movie }) => {
+  const {
+    removeMovieFromWatchlist,
+    addMovieToWatched,
+    moveToWatchlist,
+    removeFromWatched,
+  } = useContext(GlobalContext);
+
+  return (
+    <div className="inner-card-controls">
+      {type === "watchlist" && ( // Styling & buttons
+        <>
+          <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+            <i className="fa-fw far fa-eye"></i>
+          </button>
+
+          <button
+            className="ctrl-btn"
+            onClick={() => removeMovieFromWatchlist(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
+          </button>
+        </>
+      )}
+
+      {type === "watched" && ( // Styling & buttons
+        <>
+          <button className="ctrl-btn" onClick={() => moveToWatchlist(movie)}>
+            <i className="fa-fw far fa-eye-slash"></i>
+          </button>
+
+          <button
+            className="ctrl-btn"
+            onClick={() => removeFromWatched(movie.id)}
+          >
+            <i className="fa-fw fa fa-times"></i>
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
